24a4802bb3f4bad768f9b479f8f31200039f90c9
Breno Rocha <brenopenarocha2@gmail.com>
2022-12-09 19:44:06 -0300

    Botão e criação do script.js

---
+++ Sidebar/script.js
@@ -0,0 +1,7 @@
+function changeSize() {
+    const menu = document.querySelector('nav');
+    const vetLogo = document.querySelector('.vet-logo');
+    const userInfo = document.querySelector('.user-info');
+    const navFooter = document.querySelector('.nav-footer');
+    const navItems = document.querySelectorAll('.nav-item');
+}
