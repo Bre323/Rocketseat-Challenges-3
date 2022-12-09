50d480240bbde43f57fd0f809c2b792782084888
Breno Rocha <brenopenarocha2@gmail.com>
2022-12-09 20:23:43 -0300

    Sidebar Completo

--- Sidebar/script.js
+++ Sidebar/script.js
@@ -1,7 +1,14 @@
 function changeSize() {
-    const menu = document.querySelector('nav');
+    const nav = document.querySelector('nav');
     const vetLogo = document.querySelector('.vet-logo');
     const userInfo = document.querySelector('.user-info');
-    const navFooter = document.querySelector('.nav-footer');
-    const navItems = document.querySelectorAll('.nav-item');
+    const navItems = document.querySelectorAll('.item-text');
+
+    navItems.forEach((item) => {
+        item.classList.toggle('add-element')
+    })
+
+    nav.classList.toggle('add-nav');
+    vetLogo.classList.toggle('add-element');
+    userInfo.classList.toggle('add-info');
 }
