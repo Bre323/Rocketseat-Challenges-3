5ad9c3b3f766cb2f4a57c8916c3f20094f4adbcf
Breno Rocha <brenopenarocha2@gmail.com>
2023-02-06 18:39:24 -0300

    configurações da Api

---
+++ Randomflix/script.js
@@ -0,0 +1,6 @@
+const config = {
+    api_key: '0e395a22ba638fad37e18877002da327',
+    api_base_url: 'https://api.themoviedb.org/3/movie/popular?api_key=0e395a22ba638fad37e18877002da327',
+    img_base_url: 'https://image.tmdb.org/t/p/w500',
+    language: 'language=pt-BR'
+}
